19cc677c1fbf85c431e0ba2231a7c580cc858f74
agustin-noriega <agustinjoel.10f@gmail.com>
2020-05-12 19:24:05 -0300

    agregando cambios en la factory(role)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,23 +13,21 @@ class DatabaseSeeder extends Seeder
     {
 
       //SEEDERS(modificadores de DB)
-        $this->call(SeederCrearAdmin::class);
+
+      // DESCOMENTAR EL ADMIN PRIMERO PARA GENERARLO EN EL ID 1
+      //$this->call(SeederCrearAdmin::class);
+
+
        //$this->call(SeederCrearUsuario::class);
        //$this->call(SeederEliminarUsuario::class);
        //$this->call(SeederModificarUsuario::class);
 
-       //FACTORIES(crear datos en DB fakes)
+       //AGREGAR DATOS CON FACTORIES(crear datos en DB fakes) DESCOMENTAR LOS 4
+        //factory(App\User::class, 5)->create();
+        //factory(App\Post_category::class, 5)->create();
+        //factory(App\Post::class, 5)->create();
+        //factory(App\Contact::class, 5)->create();
 
-
-       // factory(App\User::class, 5)->create();
-       // factory(App\Post_category::class, 5)->create();
-       // factory(App\Post::class, 5)->create();
-       // factory(App\Contact::class, 5)->create();
-
-       //factory(App\User::class, 5)->create();
-       //factory(App\Post_category::class, 5)->create();
-       //factory(App\Post::class, 5)->create();
-       //factory(App\Contact::class, 5)->create();
 
 
     }
