5acfdd0b3b7ad6234fcc5b6ee7d4d72f3b603a56
gallol <luciano.gallo@gmail.com>
2020-05-12 17:09:16 -0300

    dataBaseseeder update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,17 +20,17 @@ class DatabaseSeeder extends Seeder
 
        //FACTORIES(crear datos en DB fakes)
 
-<<<<<<< HEAD
+
        // factory(App\User::class, 5)->create();
        // factory(App\Post_category::class, 5)->create();
        // factory(App\Post::class, 5)->create();
        // factory(App\Contact::class, 5)->create();
-=======
+
        //factory(App\User::class, 5)->create();
        //factory(App\Post_category::class, 5)->create();
        //factory(App\Post::class, 5)->create();
        //factory(App\Contact::class, 5)->create();
->>>>>>> 892f5c07943373e20403f25a1cda47cd01deda91
+
 
     }
 }
