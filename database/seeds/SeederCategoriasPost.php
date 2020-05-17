d29490a03e276897283c6529b7b9e3dbd67a75f3
agustin-noriega <agustinjoel.10f@gmail.com>
2020-05-17 17:55:03 -0300

    .

--- database/seeds/SeederCategoriasPost.php
+++ database/seeds/SeederCategoriasPost.php
@@ -11,47 +11,47 @@ class SeederCategoriasPost extends Seeder
      */
     public function run()
     {
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Acción',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Comedia',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Drama',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Suspenso',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Crimen',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Romance',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Aventura',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Terror',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Musicales',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Ciencia ficción',
 
       ]);
-      DB::table('Post_categories')->insert([
+      DB::table('post_categories')->insert([
         'category_name' => 'Guerra',
 
       ]);
