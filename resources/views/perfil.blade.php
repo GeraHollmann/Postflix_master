a03976b92a23a78dea5e969e9082586da31a9e75
gallol <luciano.gallo@gmail.com>
2020-05-21 15:27:06 -0300

    update perdil href ./

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -35,22 +35,22 @@
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="./index">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/index">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="./vistapost">Vista de posteos</a>
+        <a class="nav-link" href="/vistapost">Vista de posteos</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="./faqs">F.A.Q.S</a>
+        <a class="nav-link" href="/faqs">F.A.Q.S</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="./mis-contactos">Mis contactos</a>
+        <a class="nav-link" href="/mis-contactos">Mis contactos</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="./contacto">Ayuda</a>
+        <a class="nav-link" href="/contacto">Ayuda</a>
       </li>
     </ul>
-      <button id="search" class="btn" type="submit"><a class="btn btn-outline-success" href="./logout">Cerrar Sesion</a></button>
+      <button id="search" class="btn" type="submit"><a class="btn btn-outline-success" href="/logout">Cerrar Sesion</a></button>
   </div>
 </nav>
 
@@ -121,8 +121,8 @@
   </div>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
 
